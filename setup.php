30bd9c1b39fbc1809ddda03408ef7dfcbeedddb8
Lawrence Chen <50194219+lawrencechen0921@users.noreply.github.com>
2019-10-27 00:34:18 +0800

    Add files via upload

---
+++ setup.php
@@ -0,0 +1,6 @@
+<?php
+$db_server="inventory-db1.cpwri1ymsmqf.us-east-1.rds.amazonaws.com";			//Endpoint
+$db_user="master";		//Master name
+$db_pasw="lab-password";			//Master Password
+$db_name="inventory";	//Initial Database
+?>
